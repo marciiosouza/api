c9976ab8682760ee9985b560f397724f7da971eb
Marcio Souza <marciioasouzza@gmail.com>
2025-09-12 22:16:23 -0300

    feat: enhance route handling with regex path matching and add DELETE route for products

--- src/middlewares/routeHandler.js
+++ src/middlewares/routeHandler.js
@@ -2,10 +2,15 @@ import { routes } from "../routes/routes.js"
 
 export function routeHandler(request, response) {
   const route = routes.find((route) => {
-    return route.method === request.method && route.path === request.url
+    return route.method === request.method && route.path.test(request.url)
   })
 
   if (route) {
+    const routeParms = request.url.match(route.path)
+    const { ...params } = routeParms.groups
+
+    request.params = params
+
     return route.controller(request, response)
   }
 
